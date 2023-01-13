1a12d0d14a0f25ddc3a0ab12b78402d14f865b89
xXKen0biXx <122561739+xXKen0biXx@users.noreply.github.com>
2023-01-13 18:29:26 +0300

    Update BankingClerks.java

--- BankingClerks.java
+++ BankingClerks.java
@@ -8,7 +8,6 @@ public class Main {
         customer c3= new customer(8,2);
         customer c4= new customer(2,2);
 
-
         PriorityQueue<customer> p = new PriorityQueue<>();
         PriorityQueue<customer> p2 = new PriorityQueue<>();
         p2.add(c);
@@ -23,20 +22,41 @@ public class Main {
 
         int low = 1;
         int high = p.size();
-
         int maxWaiting = 5;
-
-
+        int ans = 0;
         while(low <= high){
             int clerks = (low+high)/2;
             PriorityQueue<customer> que = p;
             PriorityQueue<Integer> employees =  new PriorityQueue<>();
-
             for(int i = 0; i < clerks; i++){
                 employees.add(0);
 
             }
-        }
 
-    }
+            boolean exceedsWaitTime = false;
+            while (p.isEmpty() == false){
+                customer customer =p.peek();
+                p.poll();
+                int emp = employees.peek();
+                employees.poll();
+
+                if(customer.arrival < emp){
+                    int time = emp-customer.arrival;
+                    if(time> maxWaiting){
+                        exceedsWaitTime = true;
+                        break;
+                    }
+                }
+            }
+
+            if(exceedsWaitTime == true){
+                low = clerks+1;
+            }
+            else{
+
+                ans = clerks;
+                high = clerks-1;
+            }
+        }
+    }
 }
