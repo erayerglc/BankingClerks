f0a3fd3c21310305067735334892c57b3b7d8af0
MoAli7379 <81040634+MoAli7379@users.noreply.github.com>
2023-01-18 23:57:46 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,26 +1,42 @@
-public class customer {
+import java.time.LocalTime;
 
-    int  arrival;
-    int timeNeeded;
+public class Customer {
+    // time
+    LocalTime arrival;
+    //how long does it take to process a customer
+    int processingTime;
+    //unit the customer intend to go
+    public int unit;
 
-    public customer(int arrival, int timeNeeded) {
-        this.arrival = arrival;
-        this.timeNeeded = timeNeeded;
+    //constructor
+    public Customer(String arrival, int processingTime, int unit) {
+        this.arrival = LocalTime.parse(arrival);
+        this.processingTime = processingTime;
+        this.unit = unit;
     }
 
-    public int getArrival() {
+
+    //getters and setters
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+    public LocalTime getArrival() {
         return arrival;
     }
 
-    public int getTimeNeeded() {
-        return timeNeeded;
+    public int getProcessingTime() {
+        return processingTime;
     }
 
-    public void setArrival(int arrival) {
+    public void setArrival(LocalTime arrival) {
         this.arrival = arrival;
     }
 
-    public void setTimeNeeded(int timeNeeded) {
-        this.timeNeeded = timeNeeded;
-    }
+    public void setProcessingTime(int processingTime) {
+        this.processingTime = processingTime;
+    }
 }
